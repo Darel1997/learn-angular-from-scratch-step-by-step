f36a7c3ba94a978b80fc4bf960d00c23bf183408
Darel Rodriguez <darel.rodriguez@snhu.edu>
2022-06-09 14:30:56 -0400

    Module 6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://46fgh6hdyd.execute-api.us-east-1.amazonaws.com/api'
+  api_url: 'https://46fgh6hdyd.execute-api.us-east-1.amazonaws.com'
 };
